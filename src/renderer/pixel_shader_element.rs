3dd5810d821d74f27c38c1f0b64658afe382dde5
nferhat <nferhat20@gmail.com>
2025-06-06 13:19:35 +0100

    space(decorations): Rework border rendering code
    This commit addresses the rendering and logic of the borders that are
    drawn around tiles. The biggest change is that now all Tiles have a
    border field, which contains the border state and an associated
    `FhtPixelShaderElement`.
    
    Since border elements are now cached, we are properly doing damage
    tracking (see #62), which is obviously a win for performance.
    
    Another goodie introduced by this commit is border animations, they now
    smoothly transition between states, including colors, border radii, and
    thickness

---
+++ src/renderer/pixel_shader_element.rs
@@ -0,0 +1,147 @@
+use smithay::backend::renderer::element::{Element, Id, Kind, RenderElement};
+use smithay::backend::renderer::gles::element::PixelShaderElement;
+use smithay::backend::renderer::gles::{GlesError, GlesPixelProgram, Uniform};
+use smithay::backend::renderer::glow::{GlowFrame, GlowRenderer};
+use smithay::backend::renderer::utils::CommitCounter;
+use smithay::utils::{Buffer, Logical, Physical, Point, Rectangle, Scale, Transform};
+
+#[cfg(feature = "udev-backend")]
+use crate::backend::udev::{UdevFrame, UdevRenderError, UdevRenderer};
+
+/// A newtype wrapper around [`PixelShaderElement`] to allow drawing with our [`UdevRenderer`].
+#[derive(Clone, Debug)]
+pub struct FhtPixelShaderElement(PixelShaderElement);
+
+impl FhtPixelShaderElement {
+    /// Create a new [`FhtPixelShaderElement`].
+    ///
+    /// See [`PixelShaderElement::new`]
+    pub fn new(
+        program: GlesPixelProgram,
+        geometry: Rectangle<i32, Logical>,
+        alpha: f32,
+        additional_uniforms: Vec<Uniform<'static>>,
+        kind: Kind,
+    ) -> Self {
+        let inner =
+            PixelShaderElement::new(program, geometry, None, alpha, additional_uniforms, kind);
+        Self(inner)
+    }
+}
+
+impl std::ops::Deref for FhtPixelShaderElement {
+    type Target = PixelShaderElement;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl std::ops::DerefMut for FhtPixelShaderElement {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl Element for FhtPixelShaderElement {
+    fn id(&self) -> &Id {
+        self.0.id()
+    }
+
+    fn current_commit(&self) -> CommitCounter {
+        self.0.current_commit()
+    }
+
+    fn src(&self) -> Rectangle<f64, Buffer> {
+        self.0.src()
+    }
+
+    fn geometry(&self, scale: Scale<f64>) -> Rectangle<i32, Physical> {
+        self.0.geometry(scale)
+    }
+
+    fn location(&self, scale: Scale<f64>) -> Point<i32, Physical> {
+        self.geometry(scale).loc
+    }
+
+    fn transform(&self) -> Transform {
+        Transform::Normal
+    }
+
+    fn damage_since(
+        &self,
+        scale: Scale<f64>,
+        commit: Option<CommitCounter>,
+    ) -> smithay::backend::renderer::utils::DamageSet<i32, Physical> {
+        self.0.damage_since(scale, commit)
+    }
+
+    fn opaque_regions(
+        &self,
+        scale: Scale<f64>,
+    ) -> smithay::backend::renderer::utils::OpaqueRegions<i32, Physical> {
+        self.0.opaque_regions(scale)
+    }
+
+    fn alpha(&self) -> f32 {
+        self.0.alpha()
+    }
+
+    fn kind(&self) -> Kind {
+        self.0.kind()
+    }
+}
+
+impl RenderElement<GlowRenderer> for FhtPixelShaderElement {
+    fn draw(
+        &self,
+        frame: &mut GlowFrame<'_, '_>,
+        src: Rectangle<f64, Buffer>,
+        dst: Rectangle<i32, Physical>,
+        damage: &[Rectangle<i32, Physical>],
+        opaque_regions: &[Rectangle<i32, Physical>],
+    ) -> Result<(), GlesError> {
+        <PixelShaderElement as RenderElement<GlowRenderer>>::draw(
+            &self.0,
+            frame,
+            src,
+            dst,
+            damage,
+            opaque_regions,
+        )
+    }
+
+    fn underlying_storage(
+        &self,
+        _: &mut GlowRenderer,
+    ) -> Option<smithay::backend::renderer::element::UnderlyingStorage> {
+        None // pixel shader elements can't be scanned out.
+    }
+}
+
+#[cfg(feature = "udev-backend")]
+impl<'a> RenderElement<UdevRenderer<'a>> for FhtPixelShaderElement {
+    fn draw(
+        &self,
+        frame: &mut UdevFrame<'a, '_, '_>,
+        src: Rectangle<f64, Buffer>,
+        dst: Rectangle<i32, Physical>,
+        damage: &[Rectangle<i32, Physical>],
+        opaque_regions: &[Rectangle<i32, Physical>],
+    ) -> Result<(), UdevRenderError> {
+        <PixelShaderElement as RenderElement<GlowRenderer>>::draw(
+            &self.0,
+            frame.as_mut(),
+            src,
+            dst,
+            damage,
+            opaque_regions,
+        )
+        .map_err(UdevRenderError::Render)
+    }
+
+    fn underlying_storage(
+        &self,
+        _: &mut UdevRenderer<'a>,
+    ) -> Option<smithay::backend::renderer::element::UnderlyingStorage> {
+        None // pixel shader elements can't be scanned out.
+    }
+}
